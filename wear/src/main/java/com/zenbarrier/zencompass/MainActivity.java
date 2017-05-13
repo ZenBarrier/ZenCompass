9c207d651dc99ce642c8caa3fd2398c8830f9f6e
ZenBarrier <anthony.j.barrera@gmail.com>
2017-05-12 23:34:47 -0400

    Calculate degrees for compass

--- wear/src/main/java/com/zenbarrier/zencompass/MainActivity.java
+++ wear/src/main/java/com/zenbarrier/zencompass/MainActivity.java
@@ -2,6 +2,8 @@ package com.zenbarrier.zencompass;
 
 import android.content.Context;
 import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.support.wearable.activity.WearableActivity;
@@ -14,7 +16,7 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
 
-public class MainActivity extends WearableActivity {
+public class MainActivity extends WearableActivity implements SensorEventListener{
 
     private static final SimpleDateFormat AMBIENT_DATE_FORMAT =
             new SimpleDateFormat("HH:mm", Locale.US);
@@ -23,6 +25,15 @@ public class MainActivity extends WearableActivity {
     private TextView mTextView;
     private TextView mClockView;
     private SensorManager mSensorManager;
+    private Sensor mCompass;
+    private Sensor mAccelerometer;
+
+    private float[] mMagneticData;
+    private float[] mAccelerometerData;
+    private float[] mIdentityMatrix;
+    private float[] mRotationMatrix;
+    private float[] mOrientationMatrix;
+    private double mRotationDegrees = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,11 +46,27 @@ public class MainActivity extends WearableActivity {
         mClockView = (TextView) findViewById(R.id.clock);
 
         mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        if(mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD) != null){
-            Log.d("main", "has magnetic field");
-        }else{
-            Log.d("main", "no magnetic field");
-        }
+        mCompass = mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+        mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+        mMagneticData = new float[3];
+        mAccelerometerData = new float[3];
+        mIdentityMatrix = new float[9];
+        mRotationMatrix = new float[9];
+        mOrientationMatrix = new float[3];
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mSensorManager.registerListener(this, mCompass, SensorManager.SENSOR_DELAY_NORMAL);
+        mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mSensorManager.unregisterListener(this);
     }
 
     @Override
@@ -72,5 +99,27 @@ public class MainActivity extends WearableActivity {
             mTextView.setTextColor(getResources().getColor(android.R.color.black, null));
             mClockView.setVisibility(View.GONE);
         }
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        switch (event.sensor.getType()){
+            case Sensor.TYPE_ACCELEROMETER:
+                System.arraycopy(event.values, 0, mAccelerometerData, 0, 3);
+                break;
+            case Sensor.TYPE_MAGNETIC_FIELD:
+                System.arraycopy(event.values, 0, mMagneticData, 0, 3);
+                break;
+        }
+        SensorManager.getRotationMatrix(mRotationMatrix, mIdentityMatrix, mAccelerometerData, mMagneticData);
+        SensorManager.getOrientation(mRotationMatrix, mOrientationMatrix);
+        float rotationRadian = mOrientationMatrix[0];
+        mRotationDegrees = Math.toDegrees(rotationRadian);
+        mTextView.setText(mRotationDegrees+"");
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
     }
 }
